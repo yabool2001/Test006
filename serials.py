e73702ff8ff57f3be65a34035e67898ae33b5c4e
Maciek Zemlo <38382353+yabool2001@users.noreply.github.com>
2021-12-14 16:00:19 +0100

    aaaa

--- serials.py
+++ serials.py
@@ -2,14 +2,19 @@ import datetime
 from os import system
 import time
 import serial
+import struct
 
 # import sys
 # sys.setdefaultencoding('utf-8')
 
 log_file_name = 'serials.log'
 data_file_name = 'data.txt'
-conf_file_name = 'mmw_pplcount_demo_default.cfg'
+conf_file_name = 'chirp_cfg/mmw_pplcount_demo_default.cfg'
+# conf_file_name = 'chirp_cfg/sense_and_direct_68xx.cfg'
+# conf_file_name = 'chirp_cfg/long_range_people_counting.cfg'
 data_com_delta_seconds = 10
+frame_header_length = 52
+data_byte = bytes(1)
 
 conf_com = serial.Serial ()
 data_com = serial.Serial ()
@@ -94,30 +99,27 @@ else:
 ################################################################
 try: 
     data_com.open ()
+    if data_com.is_open:
+        log_file.write ( f'\n{time.gmtime ().tm_hour}:{time.gmtime ().tm_min}:{time.gmtime ().tm_sec} {data_com.name} port opened' )
+    data_com.reset_output_buffer()
+    # time_up = datetime.datetime.utcnow () + datetime.timedelta ( seconds = data_com_delta_seconds )
+    # while datetime.datetime.utcnow () < time_up :
+        # Jedna z wersji odczytująca dane ale generująca błędy podczas decode()
+        # data_line = data_com.readline ()
+        # data_file.write ( str ( data_line ) )
+        # lub wersja usuwająca znak końca linii
+        # data_line = data_com.readline ()[:-1] # [:-1] czyli bez znaku końca linii z powodu, któego by nie działał decode()
+        # data_file.write ( data_line.decode () )
+    data_frame = data_com.read ( 4666 )
+    # data_byte = data_frame
+    # magic, version, platform, timestamp, packetLength, frameNum, subFrameNum, chirpMargin, frameMargin, uartSentTime, trackProcessTime, numTLVs, checksum = struct.unpack('Q10I2H', data_frame[:frame_header_length])
+    sync , version ,  platform , timestamp , packet_length , frame_number , subframe_number , chirp_margin , frame_margin , uart_sent_time , track_process_time , num_tlvs , checksum = struct.unpack ('Q10I2H', data_frame[:frame_header_length])
+
+    x=1
+    # data_file.write ( str ( data_frame ) )
+    #data_file.write ( data_frame.decode() )
 except serial.SerialException as e:
     log_file.write ( f'\n{time.gmtime ().tm_hour}:{time.gmtime ().tm_min}:{time.gmtime ().tm_sec} {data_com.name} port error opening: {str(e)}' )
-if data_com.is_open:
-    log_file.write ( f'\n{time.gmtime ().tm_hour}:{time.gmtime ().tm_min}:{time.gmtime ().tm_sec} {data_com.name} port opened' )
-    #data_com.reset_output_buffer()
-    #try:
-    #    data_com.write ( str.encode ( f'UTC {time.gmtime ().tm_hour}:{time.gmtime ().tm_min}:{time.gmtime ().tm_sec}\r\n' ) ) 
-    #except serial.SerialTimeoutException as e:
-    #    print ( f'error write {data_com.name}: {str(e)}' )
-
-#time_up = datetime.datetime.utcnow () + datetime.timedelta ( seconds = data_com_delta_seconds )
-#while datetime.datetime.utcnow () < time_up :
-    # data_line = data_com.readline ()[:-1] # [:-1] czyli bez znaku końca linii z powodu, któego by nie działał decode()
-    # data_file.write ( data_line.decode () )
-    
-    # Jedna z wersji odczytująca dane ale generująca błędy podczas decode()
-    # data_line = data_com.readline ()
-    # data_file.write ( str ( data_line ) )
-    
-    # Wersja alternatywna odczytująca dane ale nie po linii tylko po jednym bajcie, zeby sprawdzić czy zadziała funkcja decode()
-data_byte = data_com.read ( 4666 )
-data_file.write ( str ( data_byte ) )
-    #data_file.write ( data_byte.decode() )
-
 
 try:
     data_com.close ()
